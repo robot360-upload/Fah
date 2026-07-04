5e03631c8f49092d45e1f3f1e6c63a0e46e15bdf
robot360-upload <angelopaulsxcmi3b@gmail.com>
2026-07-04 23:21:07 +0530

    Add HookProvider class extending ContentProvider

---
+++ app/src/main/java/hook/call/HookProvider.java
@@ -0,0 +1,62 @@
+package hook.call;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+
+/*
+ * Created by aantik
+ * 6/07/2026 1:40 PM
+ *
+ *   ⋆    ႔ ႔
+ *     ᠸ^ ^ ⸝⸝
+ *       |、˜〵
+ *      じしˍ,)⁐̤ᐷ
+ *
+ * Fox Mode 🍺
+ */
+
+
+public class HookProvider extends ContentProvider {
+    @Override
+    public boolean onCreate() {
+        try {
+            Hooker.onMethodCalled(null);
+        } catch (Exception e) {
+        }
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        return null;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
+
